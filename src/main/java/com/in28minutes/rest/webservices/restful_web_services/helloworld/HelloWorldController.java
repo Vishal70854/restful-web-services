25142339c8bd02b5b19e914f36abf8dd27e0ac49
Vishal Kumar Singh <vishalkumar70854@gmail.com>
2025-09-02 07:15:53 +0530

    Internationalization(i18n) to get response in different languages

--- src/main/java/com/in28minutes/rest/webservices/restful_web_services/helloworld/HelloWorldController.java
+++ src/main/java/com/in28minutes/rest/webservices/restful_web_services/helloworld/HelloWorldController.java
@@ -1,12 +1,22 @@
 package com.in28minutes.rest.webservices.restful_web_services.helloworld;
 
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Locale;
+
 @RestController
 public class HelloWorldController {
+    // to get response in different languages
+    private MessageSource messageSource;
+
+    public HelloWorldController(MessageSource messageSource){
+        this.messageSource = messageSource;
+    }
 
     @GetMapping("/hello-world")
     public String helloWorld(){
@@ -21,5 +31,12 @@ public class HelloWorldController {
     @GetMapping("/hello-world/path-variable/{name}")
     public HelloWorldBean helloWorldPathVariable(@PathVariable String name){
         return new HelloWorldBean("Hello World - " + name);
+    }
+
+    // implementing internationalization(i18n)
+    @GetMapping("/hello-world-internationalized")
+    public String helloWorldInternationalized(){
+        Locale locale = LocaleContextHolder.getLocale();
+        return messageSource.getMessage("good.morning.message",null, "Default Message", locale);
     }
 }
